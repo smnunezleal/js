0670e8e9ebe94f8c1662db40c3dd800febf59ae1
Sebastian Matias Nuñez Leal <sebastian1986@gmail.com>
2022-01-14 10:31:24 -0300

    test

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+//My First Sample
 console.log("Hi Earth!");
 console.log("this is my change");
+console.log("hey what are you doing?");
+
+let varName = "MyVar";
+let varName2 = "2"
+let final = `This is my final version: ${varName} - ${varName2}`
+console.log(final);
+console.log(typeof final);
