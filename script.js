6153f5ac067e0e934ff16ccf343984a9fb0142c6
Sebastian Matias Nuñez Leal <sebastian.matias.nunez.leal@outlook.com>
2022-01-14 10:31:49 -0300

    tests

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 console.log("Hi Earth!");
 console.log("this is my change");
+
+function eat(food1, food2)
+{
+    alert("I like to eat " + food1 + " and " + food2 );
+}
+/* function myFunc(callback, args)
+{
+    //do stuff
+    //...
+    //execute callback when finished
+    callback.apply(this, args);
+}
+
+//alerts "I like to eat pickles and peanut butter"
+myFunc(eat, ["pickles", "peanut butter"]); 
+ */
+function func1(a, b){
+    alert(`Results: ${a} and ${b}`);
+}
+
+function func2(callback, args)
+{
+    callback.apply(this,args);
+}
+
+func2(func1, ["A","B"]);
+
